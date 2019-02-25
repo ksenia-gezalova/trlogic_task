9bc18ab9815ecafbbb32966ae7b4e1b1347766f0
ksenia-gezalova <ksenia.gezalova@gmail.com>
2019-02-25 11:28:34 +0300

    rm unnecessary code

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-//import db from './datastore'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-//Vue.prototype.$db = db
